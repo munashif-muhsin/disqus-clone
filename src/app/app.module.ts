cddc6e7c6daedb5882d1515b1704d34c734e821d
Muhsin <munashif.muhsin@gmail.com>
2019-06-17 19:13:31 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,72 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {
+  BrowserModule
+} from '@angular/platform-browser';
+import {
+  NgModule
+} from '@angular/core';
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {
+  AppComponent
+} from './app.component';
+import {
+  HomeComponent
+} from './pages/home/home.component';
+import {
+  AdminComponent
+} from './pages/admin/admin.component';
+import {
+  HeaderComponent
+} from './modules/header/header.component';
+import { FooterComponent } from './modules/footer/footer.component';
+import { PostComponent } from './pages/post/post.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'admin',
+    component: AdminComponent,
+    data: {
+      title: 'Admin - Voter Circle'
+    }
+  },
+  {
+    path: 'post',
+    component: PostComponent,
+    data: {
+      title: 'Post - Voter Circle'
+    }
+  },
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    HeaderComponent,
+    FooterComponent,
+    PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes, {
+        enableTracing: false
+      } // <-- debugging purposes only
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
